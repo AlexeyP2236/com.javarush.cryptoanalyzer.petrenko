a0f13ede16240ddee2d60683d930287ccb57a7a9
Alexey Petrenko <loki2236@mail.ru>
2023-12-28 08:08:10 +0300

    смена репозитория

---
+++ src/TextEncryption.java
@@ -0,0 +1,42 @@
+public class TextEncryption {
+    private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
+            'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+
+    public String readerFile(String url, int shift, int mode) {
+        char[] charText = WorkingWithFile.readerFile(url);
+        return encryption(charText, shift, mode);
+    }
+
+    private String encryption(char[] text, int shift, int mode) {
+        if (shift > ALPHABET.length) shift = shift % ALPHABET.length;
+        char[] arrChar = new char[text.length];
+        int count = 0;
+        for (int i = 0; i < text.length; i++) {
+            arrChar[i] = text[i];
+            for (int j = 0; j < ALPHABET.length; j++) {
+                if (count + shift == ALPHABET.length) count = 0 - shift;
+                if (mode == 1){
+                    if (ALPHABET[j] == Character.toLowerCase(text[i])) {
+                        if (ALPHABET[j] == text[i]) arrChar[i] = ALPHABET[count + shift];
+                        else arrChar[i] = Character.toUpperCase(ALPHABET[count + shift]);
+                        break;
+                    }
+                }
+                else if (mode == 2){
+                    if (ALPHABET[count + shift] == Character.toLowerCase(text[i])) {
+                        if (ALPHABET[count + shift] == text[i]) arrChar[i] = ALPHABET[j];
+                        else arrChar[i] = Character.toUpperCase(ALPHABET[j]);
+                        break;
+                    }
+                }
+                count++;
+            }
+            count = 0;
+        }
+        System.out.println();
+        String result = new String(arrChar);
+        System.out.println(result);
+        return result;
+    }
+}
---
+++ src/WorkingWithFile.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WorkingWithFile {
+    public static char[] readerFile(String url) {
+        char[] charText = new char[url.length()];
+        try {
+            Path fileName = Path.of(url);
+            String inStreamString = Files.readString(fileName);
+            System.out.println(inStreamString);
+            charText = inStreamString.toCharArray();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return charText;
+    }
+
+    public static void writeFile(String directory, String code, int modeWrite) {
+        String urlCopy = "";
+        if (modeWrite == 1) urlCopy = directory + "-code.txt";
+        else if (modeWrite == 2) urlCopy = directory + "-decode.txt";
+            try {
+                Path path = Path.of(urlCopy);
+                Files.writeString(path, code);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+    }
+}
