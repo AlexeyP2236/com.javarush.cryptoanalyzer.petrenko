7c625d3d530aa1cf2f7ef8c042db753daedbbde3
Alexey Petrenko <loki2236@mail.ru>
2023-12-28 10:31:08 +0300

    Добавление расшифровки интерфейса

---
+++ src/Interface.java
@@ -0,0 +1,33 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Interface {
+    public static void interfaceEncryption(int mode){
+        Scanner in = new Scanner(System.in);
+        // "C:\code\";
+        String fileName;
+        TextEncryption encryption = new TextEncryption();
+        String url = "C:\\code\\";
+        while (true) {
+            System.out.println("\nВведите имя и путь к текстовому файлу: (пример C:\\code\\file, exit чтобы вернутся в меню)");
+            fileName = in.nextLine();
+            if (Path.of(fileName).isAbsolute()) {
+                url = fileName + ".txt";
+                if (Files.isRegularFile(Path.of(url))) break;
+                else System.out.println("С таким именем файла не существует");
+            } else if (fileName.equals("exit")) {
+                break;
+            } else System.out.println("Некорректный ввод");
+        }
+        System.out.println("Введите числовой ключ:");
+        if (in.hasNextInt()) {
+            int key = in.nextInt();
+            WorkingWithFile.writeFile(fileName, encryption.readerFile(url, key, mode), mode);
+            if (mode == 1) System.out.println("Шифровка завершена\n" + "-".repeat(100));
+            if (mode == 2) System.out.println("Расшифровка завершена\n" + "-".repeat(100));
+        } else {
+            System.out.println("Некорректный ввод\n");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,21 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         while (true) {
             Scanner in = new Scanner(System.in);
-            System.out.println("Введите число\n" +
+            System.out.println("Введите число:\n" +
                     "1 - Сделать шифровку;\n" +
                     "2 - Расшифровать файл;\n" +
-                    "3 - ;\n" +
+                    "3 - Взломать файл;\n" +
                     "4 - exit");
             String console = in.nextLine();
             if (console.equals("1")) {
                 int mode = 1;
-                // "C:\code\";
-                String directory;
-                //проверка о существовании директории
-                //проверка о существовании файла
-                String url = "C:\\code\\";
-                while (true) {
-                    System.out.println("\nВведите путь к текстовому файлу: (пример C:\\code\\file, exit чтобы вернутся в меню)");
-                    directory = in.nextLine();
-                    if (Path.of(directory).isAbsolute()) {
-                        url = directory + ".txt";
-                        if (Files.isRegularFile(Path.of(url))) break;
-                        else System.out.println("Некорректный ввод");
-                    } else if (directory.equals("exit")) {
-                        break;
-                    } else System.out.println("Некорректный ввод");
-                }
-                TextEncryption encryption = new TextEncryption();
-                System.out.println("Введите числовой ключ:");
-                if (in.hasNextInt()) {
-                    int key = in.nextInt();
-                    WorkingWithFile.writeFile(directory, encryption.readerFile(url, key, mode), mode);
-                    System.out.println("Шифровка завершена\n" + "-".repeat(100));
-                } else {
-                    System.out.println("Некорректный ввод\n");
-                }
-
-
+                Interface.interfaceEncryption(mode);
             } else if (console.equals("2")) {
-//                int mode = 2;
-//                String nameFile = "file-code";
-//                String directory = "C:\\code\\";
-//                String url = directory + nameFile + ".txt";
-//                TextEncryption encryption = new TextEncryption();
-//                WorkingWithFile.writeFile(directory, encryption.readerFile(url, 17, mode), mode);
-//                System.out.println("Расшифровка завершена\n");
-
+                int mode = 2;
+                Interface.interfaceEncryption(mode);
             } else if (console.equals("3")) {
 
             } else if (console.equals("4") || console.equals("exit")) break;
